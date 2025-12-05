19ab912488721fbbdc765401fc78ee988c304662
Ansh <ayush5798ji@gmail.com>
2025-12-05 18:47:15 +0530

    removed box1 and box2.js

---
+++ उगता_हुआ_धुआँ.js
@@ -0,0 +1,8 @@
+let body = document.querySelectorAll("body");
+let mq = window.matchMedia("(max-width: 1285px)");
+let box1 = document.querySelector(".box1");
+let box3 = document.querySelector(".box3");
+let box2 = document.querySelector(".box2");
+box1.remove();
+box3.remove();
+box2.style.width="85%";
