2cd453ca0b7e91564eccbd979e77749dac945d88
Ansh <ayush5798ji@gmail.com>
2025-12-05 19:30:55 +0530

    language select.js

--- firstnobel.js
+++ firstnobel.js
@@ -1,31 +1,27 @@
-// let box1 = document.querySelector(".box1");
-// let box2 = document.querySelector(".box2");
-// let box3 = document.querySelector(".box3");
+let box1 = document.querySelector(".box1");
+let box2 = document.querySelector(".box2");
+let box3 = document.querySelector(".box3");
+let body = document.querySelector("body");
+let content = document.querySelectorAll(".content");
 // let footer = document.querySelectorAll("footer");
-// let lang = prompt("Enter the language in which you wants to read English or Hindi :");
-// if(lang === "English" || lang === "english" ){
-//     box3.style.visibility = 'hidden';
-//     footer.style.marginTop = "1871px";
-// }else if(lang === "Hindi" || lang === "hindi" ){
-//     box1.style.visibility = 'hidden';
-//     box2.style.marginTop = '10px';
-// }else{
-//     alert("Sorry! Has no version in that language");
-//     box1.style.visibility = 'hidden';
-//     box3.style.visibility = 'hidden';
-// }
+let lang = prompt("Enter the language in which you wants to read English or Hindi :");
+if(lang === "English" || lang === "english" ){
+    box3.remove();
+    alert("For Reading in Hindi reload the page and enter language Hindi ");
+}else if(lang === "Hindi" || lang === "hindi" ){
+    // box1.remove();
+    box1.replaceWith(box3);
+    alert("For Reading in English reload the page and enter language English ");
+}else{
+    alert("Sorry! Found no version in that language");
+    box1.remove();
+    box3.remove();
+}
 document.querySelectorAll(".under a").forEach(link => {
     link.addEventListener("click", ()=>{
         link.style.color = "red";
         link.style.fontWeight = "700";
     });
-});
-window.addEventListener("scroll", ()=>{
-    if(window.scrollY > 300){
-        topbtn.style.display = "block";
-    } else {
-        topbtn.style.display = "none";
-    }
 });
 // .under li:hover {
 //     transform: translateX(5px);
